38dcdf10113cd2fe17bc450106f23fe843df23f5
Gabriel Nahum <gnahum12345@gmail.com>
2016-04-26 10:19:56 -0700

    Initial Commit

---
+++ C-Color/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  C-Color
+//
+//  Created by Nelson Nahum on 4/26/16.
+//  Copyright © 2016 Gabriel Nahum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ C-Color/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C-Color
+//
+//  Created by Nelson Nahum on 4/26/16.
+//  Copyright © 2016 Gabriel Nahum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
